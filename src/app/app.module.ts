149eb15750541e531a4c1dafd24e5a71936e59ad
Karol Arakelian <kaholavan@hotmail.com>
2019-08-29 16:08:11 +0300

    added cryappter module | npm install required

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,41 +12,42 @@ import { SailsModule, SailsOptions, SailsEnvironment } from 'ngx-sails-socketio'
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { environment } from './../environments/environment';
+import { CryappterModule } from 'cryappter';
 
-import {NgxFileDropModule} from 'ngx-file-drop';
+import { NgxFileDropModule } from 'ngx-file-drop';
 // Sails Options
 const sailsOptions: SailsOptions = {
-  url: environment.webSocketUrl,
-  prefix: '',
-  environment: SailsEnvironment['PROD'],
-  // environment: SailsEnvironment[environment.production ? 'PROD' : 'DEV'],
-  query: '__sails_io_sdk_version=0.11.0&__sails_io_sdk_platform=windows&__sails_io_sdk_language=javascript',
-  reconnection: true,
-  autoConnect: false,
-  rejectUnauthorized: false,
-  // headers: {
-  //     // tslint:disable-next-line:max-line-length
-  //     Authorization: localStorage.getItem("token")
-  // },
-  // timeout: 3000,
+	url: environment.webSocketUrl,
+	prefix: '',
+	environment: SailsEnvironment['PROD'],
+	// environment: SailsEnvironment[environment.production ? 'PROD' : 'DEV'],
+	query: '__sails_io_sdk_version=0.11.0&__sails_io_sdk_platform=windows&__sails_io_sdk_language=javascript',
+	reconnection: true,
+	autoConnect: false,
+	rejectUnauthorized: false,
+	// headers: {
+	//     // tslint:disable-next-line:max-line-length
+	//     Authorization: localStorage.getItem("token")
+	// },
+	// timeout: 3000,
 };
 // Directives
 import { DndDirective, DisableControlDirective } from '@directives';
 
 // Services
-import { ApiService, SocketService, ErrorService, FileService,INTERCEPTORS} from '@services';
+import { ApiService, SocketService, ErrorService, FileService, INTERCEPTORS } from '@services';
 
 // Components
 import {
-  WelcomeComponent,
-  ForgotPasswordComponent,
-  ChangePasswordComponent,
-  LoginComponent,
-  RegisterComponent,
-  HomeComponent,
-  DragAndDropComponent,
-  TransfersComponent,
-  UserSettingsComponent
+	WelcomeComponent,
+	ForgotPasswordComponent,
+	ChangePasswordComponent,
+	LoginComponent,
+	RegisterComponent,
+	HomeComponent,
+	DragAndDropComponent,
+	TransfersComponent,
+	UserSettingsComponent
 } from '@components';
 import { PrivacyPolicyDialog, TermsOfUseDialog } from './components/register/register.component';
 import { ResetPasswordComponent } from './components/home/menu-items/user-settings/reset-password/reset-password.component';
@@ -62,70 +63,71 @@ import { TransferMethodDialogComponent } from './components/home/send-page/trans
 
 // Translate loader with AOT
 export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+	return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DndDirective,
-    DisableControlDirective,
-    WelcomeComponent,
-    LoginComponent,
-    RegisterComponent,
-    ForgotPasswordComponent,
-    ChangePasswordComponent,
-    PrivacyPolicyDialog,
-    TermsOfUseDialog,
-    HomeComponent,
-    DragAndDropComponent,
-    TransfersComponent,
-    UserSettingsComponent,
-    ResetPasswordComponent,
-    HistoryComponent,
-    ConfirmDialogComponent,
-    SendPageComponent,
-    FileSizePipe,
-    ContactsDialogComponent,
-    NonCopaUsersDialogComponent,
-    MultiFactorAuthenticationDialogComponent,
-    CreateLinkDialogComponent,
-    TransferMethodDialogComponent,
-  ],
-  entryComponents: [
-    PrivacyPolicyDialog,
-    TermsOfUseDialog,
-    TransfersComponent,
-    UserSettingsComponent,
-    ResetPasswordComponent,
-    HistoryComponent,
-    ConfirmDialogComponent,
-    ContactsDialogComponent,
-    NonCopaUsersDialogComponent,
-    MultiFactorAuthenticationDialogComponent,
-    CreateLinkDialogComponent,
-    TransferMethodDialogComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-    FlexLayoutModule,
-    NgxFileDropModule,
-    SailsModule.forRoot(sailsOptions, INTERCEPTORS),
-    TranslateModule.forRoot({
-      loader: {
-          provide: TranslateLoader,
-          useFactory: createTranslateLoader,
-          deps: [HttpClient]
-      }
-  })
-  ],
-  providers: [ApiService, FileService, SocketService, AuthGuard, ErrorService],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		DndDirective,
+		DisableControlDirective,
+		WelcomeComponent,
+		LoginComponent,
+		RegisterComponent,
+		ForgotPasswordComponent,
+		ChangePasswordComponent,
+		PrivacyPolicyDialog,
+		TermsOfUseDialog,
+		HomeComponent,
+		DragAndDropComponent,
+		TransfersComponent,
+		UserSettingsComponent,
+		ResetPasswordComponent,
+		HistoryComponent,
+		ConfirmDialogComponent,
+		SendPageComponent,
+		FileSizePipe,
+		ContactsDialogComponent,
+		NonCopaUsersDialogComponent,
+		MultiFactorAuthenticationDialogComponent,
+		CreateLinkDialogComponent,
+		TransferMethodDialogComponent,
+	],
+	entryComponents: [
+		PrivacyPolicyDialog,
+		TermsOfUseDialog,
+		TransfersComponent,
+		UserSettingsComponent,
+		ResetPasswordComponent,
+		HistoryComponent,
+		ConfirmDialogComponent,
+		ContactsDialogComponent,
+		NonCopaUsersDialogComponent,
+		MultiFactorAuthenticationDialogComponent,
+		CreateLinkDialogComponent,
+		TransferMethodDialogComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		HttpClientModule,
+		FormsModule,
+		ReactiveFormsModule,
+		BrowserAnimationsModule,
+		MaterialModule,
+		FlexLayoutModule,
+		NgxFileDropModule,
+		SailsModule.forRoot(sailsOptions, INTERCEPTORS),
+		TranslateModule.forRoot({
+			loader: {
+				provide: TranslateLoader,
+				useFactory: createTranslateLoader,
+				deps: [HttpClient]
+			}
+		}),
+		CryappterModule
+	],
+	providers: [ApiService, FileService, SocketService, AuthGuard, ErrorService],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
